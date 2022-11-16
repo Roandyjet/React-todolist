bda2b232ea31edc89e3d6ee43e88ee462775ccb9
Rova <roandyjet@gmail.com>
2022-11-16 22:04:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [textInput, setTextInput] = useState("");
+  const [lists, setLists] = useState([]);
+
+  function handleChange(e) {
+    const newList = e.target.value;
+    setTextInput(newList);
+  }
+
+  function addList(e) {
+    setLists((prevList) => {
+      return [...prevList, textInput];
+    });
+
+    setTextInput("");
+
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <form action="">
+        <div className="form">
+          <input type="text" onChange={handleChange} value={textInput} />
+          <button type="submit" onClick={addList}>
+            <span>Add</span>
+          </button>
+        </div>
+      </form>
+      <div>
+        <ul>
+          {lists.map((list, index) => {
+            return <li key={index}>{list}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
